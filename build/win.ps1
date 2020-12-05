025f3e92c956da62497728b2cfc07b551275bfb3
Fei <33122873+pheihuihui@users.noreply.github.com>
2020-12-05 18:20:30 +0800

    ...

--- build/win.ps1
+++
@@ -1,3 +0,0 @@
-$pathArr = Get-ChildItem .\src\content\ | Select-Object -expand Name
-$paths = $pathArr -join ","
-webpack --env paths=$paths
--- src/content/about/index.ts
+++
@@ -1,7 +0,0 @@
-let a = 1
-
-let div = document.createElement('div')
-div.innerText = 'hello'
-document.body.appendChild(div)
-
-export { }
